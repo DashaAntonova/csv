6373850f5e0444a7e1a6d540be56b12d7bdcc32b
DashaAntonova <Miabelika@gmail.com>
2017-02-20 13:01:47 +0500

    answers_to_comments

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE ScopedTypeVariables, DeriveGeneric #-}
-
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 import GHC.Generics
 import Data.Foldable (traverse_)
 import Data.List
-import Data.Time    --использую Time, а не dateTime. Ишус про импорты
+import Data.Time
 import Data.Maybe
-import Data.Tuple.HT
+--import Data.Tuple.HT
 
 {-Помню, когда-то ты говорил, что нужно юзать не String, а Text. Здесь не стала так делать,
 потому что в функции parseTimeM как раз String, а не Text. Или нужно как-то по-другому поступить?-}
@@ -28,14 +27,11 @@ valuesToList (Data_csv a b) = Pomodoros dayFromCsv b
                                       localTime = zonedTimeToLocalTime (fromJust zonedTime)
                                       zonedTime = (parseTimeM True defaultTimeLocale iso8601 a :: Maybe ZonedTime)
                                     
-{-В этой функции не знала, как быть с Left err. В итоге сделала очень криво... В случае ошибки
-возвращаю какое-то странное значение, просто для того, чтобы вернуть хоть что-то, соответствующее типу. 
-Не придумала, как тут быть. Может быть, нужно использовать Maybe?-}
-listFromCsv :: BL.ByteString -> [Data_csv]
+listFromCsv :: BL.ByteString -> IO [Data_csv]
 listFromCsv csvData = 
     case decode NoHeader csvData of
-        Left err -> [(Data_csv err 0)]
-        Right v -> V.toList v
+        Left err -> fail err
+        Right v -> return $ V.toList v
 
 sumPomodoros :: [Pomodoros] -> Pomodoros
 sumPomodoros array = Pomodoros (day (head array)) sumArray
@@ -50,11 +46,11 @@ iso8601 = iso8601DateFormat $ Just "%H:%M:%S%Q%z"
 main :: IO ()
 main = do
     csvData <- BL.readFile "pomodoros.csv"
-    let list = map valuesToList (listFromCsv csvData)
+    list <- fmap (map valuesToList) $ listFromCsv csvData
     --traverse_ print list
     
     --вот, получила список дней с количеством секунд
     let groupList = map sumPomodoros (groupBy equalPomodoros list)
     traverse_ print groupList
-            
+
     putStrLn "End"
