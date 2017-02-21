1d9a1c47cd6c8652c68fe39129097539783aa732
DashaAntonova <Miabelika@gmail.com>
2017-02-21 14:40:16 +0500

    Changes ValueToList var1

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,6 @@ import Data.Time
 import Data.Maybe
 --import Data.Tuple.HT
 
-{-Помню, когда-то ты говорил, что нужно юзать не String, а Text. Здесь не стала так делать,
-потому что в функции parseTimeM как раз String, а не Text. Или нужно как-то по-другому поступить?-}
 data Data_csv = Data_csv String Int deriving (Generic, Show)
 
 data Pomodoros = Pomodoros {
@@ -21,11 +19,16 @@ data Pomodoros = Pomodoros {
 instance FromRecord Data_csv
 instance ToRecord Data_csv
 
-valuesToList :: Data_csv -> Pomodoros
-valuesToList (Data_csv a b) = Pomodoros dayFromCsv b
-                                where dayFromCsv = localDay localTime
-                                      localTime = zonedTimeToLocalTime (fromJust zonedTime)
-                                      zonedTime = (parseTimeM True defaultTimeLocale iso8601 a :: Maybe ZonedTime)
+valuesToList :: Data_csv -> Maybe Pomodoros
+valuesToList (Data_csv a b) = case (parseTimeM True defaultTimeLocale iso8601 a :: Maybe ZonedTime) of
+                              Nothing -> Nothing
+                              isJust -> Just $ Pomodoros dayFromCsv b
+                                        where dayFromCsv = localDay localTime
+                                              localTime = zonedTimeToLocalTime (fromJust zonedTime)
+                                              zonedTime = (parseTimeM True defaultTimeLocale iso8601 a :: Maybe ZonedTime)
+
+test :: Data_csv -> Maybe ZonedTime
+test (Data_csv a b) = (parseTimeM True defaultTimeLocale iso8601 a :: Maybe ZonedTime)
                                     
 listFromCsv :: BL.ByteString -> IO [Data_csv]
 listFromCsv csvData = 
@@ -33,12 +36,16 @@ listFromCsv csvData =
         Left err -> fail err
         Right v -> return $ V.toList v
 
-sumPomodoros :: [Pomodoros] -> Pomodoros
-sumPomodoros array = Pomodoros (day (head array)) sumArray
-                where sumArray = foldl (\x (Pomodoros a b) -> x+b) 0 array
+sumPomodoros :: [Maybe Pomodoros] -> Maybe Pomodoros
+sumPomodoros array = case head array of
+                     Nothing -> Nothing
+                     isJust -> Just $ Pomodoros (day $ fromJust (head array)) sumArray
+                               where sumArray = foldl (\x (Pomodoros a b) -> x+b) 0 (catMaybes array)
 
-equalPomodoros :: Pomodoros -> Pomodoros -> Bool
-equalPomodoros (Pomodoros x y) (Pomodoros a b) = if x == a then True else False
+equalPomodoros :: Maybe Pomodoros -> Maybe Pomodoros -> Bool
+equalPomodoros Nothing Nothing = True
+equalPomodoros (Just (Pomodoros x y)) (Just (Pomodoros a b)) = if x == a then True else False
+equalPomodoros _ _ = False
 
 iso8601 :: String
 iso8601 = iso8601DateFormat $ Just "%H:%M:%S%Q%z"
@@ -48,8 +55,8 @@ main = do
     csvData <- BL.readFile "pomodoros.csv"
     list <- fmap (map valuesToList) $ listFromCsv csvData
     --traverse_ print list
+    --putStrLn "End1"
     
-    --вот, получила список дней с количеством секунд
     let groupList = map sumPomodoros (groupBy equalPomodoros list)
     traverse_ print groupList
 
